173d675e7f5250e5a7190c4bedee0327ad6fe461
Alex-Rabaev <rabaevav@live.com>
2025-08-07 07:02:56 +0300

    fix: small addings

--- ai-business-buddy-bot/app/workflows/activities/messaging.py
+++ ai-business-buddy-bot/app/workflows/activities/messaging.py
@@ -9,4 +9,8 @@ async def send_message(chat_id: int, text: str):
     load_dotenv()
     bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
     print(f"[SEND_MESSAGE] Sending message to {chat_id}: {text}")
-    await bot.send_message(chat_id, text)
+    
+    try:
+        await bot.send_message(chat_id, text)
+    finally:
+        await bot.session.close()
